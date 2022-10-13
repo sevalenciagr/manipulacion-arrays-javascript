c8ee0abecb8de8978ea83706b224672d3656f731
Sebastián Valencia G <sevalenciagr@gmail.com>
2022-10-12 22:11:53 -0500

    mutable sort

--- server/15-mutable.js
+++ server/15-mutable.js
@@ -18,3 +18,26 @@ if (productIndex !== -1){
 console.log("products", products);
 console.log("myProducts", myProducts);
 console.log("-".repeat(10));
+
+/*Update*/
+
+const productsV2 = [
+    { title: 'Pizza', price: 121, id: '🍕' },
+    { title: 'Burger', price: 121, id: '🍔' },
+    { title: 'Hot cakes', price: 121, id: '🥞' },
+];
+
+const update = {
+    id: '🥞',
+    changes: {
+        price: 200,
+        descrption: 'delicioso'
+    }
+}
+
+const productIndexV2 = products.findIndex(item => item.id === update.id);
+productsV2[productIndexV2] = {
+    ...productsV2[productIndexV2],
+    ...update.changes,
+};
+console.log(productsV2);
